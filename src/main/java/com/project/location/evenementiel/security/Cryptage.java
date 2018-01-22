36fb22360091a2c8485c3c12f7d786ad62bef358
Diary <diary.rasolofomanana@gmail.com>
2018-01-23 01:08:56 +0300

    add class cryptage

---
+++ src/main/java/com/project/location/evenementiel/security/Cryptage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.location.evenementiel.security;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author diary
+ */
+public class Cryptage {
+    public static String crypterHashage(String password) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(password.getBytes());
+        byte byteData[] = md.digest();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        return sb.toString();
+    }
+}
